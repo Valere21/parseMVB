d552a41b2db6fe5b43c3491671f31a84bbbdf50a
valere21 <valere.gh@hotmail.fr>
2021-10-15 13:53:18 +0200

    all

--- csvfile.cpp
+++ csvfile.cpp
@@ -4,10 +4,6 @@
 using namespace std;
 CSVFile::CSVFile()
 {
-
-    QFile *m_fileRef = new QFile("C://Users//Valere.gay-heuzey//OneDrive - RATP Dev//Documents//RATP DEV//RailNova//telematics.csv");
-    m_fileRef->open(QIODevice::ReadWrite);
-    getCSVRef("C://Users//Valere.gay-heuzey//OneDrive - RATP Dev//Documents//RATP DEV//RailNova//telematics.csv");
     fillListColumnName();
 }
 
@@ -39,14 +35,28 @@ void CSVFile::fillListColumnName(){
     QFile *m_fileRef = new QFile("C://Users//Valere.gay-heuzey//OneDrive - RATP Dev//Documents//RATP DEV//RailNova//telematics.csv");
     m_fileRef->open((QIODevice::ReadWrite));
     QByteArray line = m_fileRef->readLine();
-
     line = line.remove(line.indexOf('\n'), line.size());
-    while (!(line.isEmpty())){
-        m_listColumnName.append(line.mid(0, line.indexOf(';')));
-        qDebug() << line << "aahh";
-        if (line.contains(';')) line.remove(0,line.indexOf(';'));
-        else break;
+
+    while(line.contains(';') || line.contains('\n')){
+        QString *newColumn = new QString((line.remove(line.indexOf(';'),-1)));
+        line = line.remove(0,line.indexOf(';')+1);
+        m_listColumnName.append(*newColumn);
+        qDebug() << "aaa" << *newColumn;
+        delete newColumn;
     }
-    qDebug() << "stop";
-    qDebug() << m_listColumnName.at(1);
+//    line = line.remove(line.indexOf('\n'), line.size());
+//    while (!(line.isEmpty())){
+//        m_listColumnName.append(line.mid(0, line.indexOf(';')));
+//        qDebug() << line << "aahh";
+//        if (line.contains(';')) line.remove(0,line.indexOf(';'));
+//        else break;
+//    }
+
+    qDebug() << "stop " << m_listColumnName.at(1);
 }
+
+
+
+
+
+
